e960bc49807e7e516f30db79d2f5520a01eaf9fe
Mike Hewitt <matroxmike@icloud.com>
2017-07-22 22:38:41 +1000

    small changes

--- Hwhoami/src/Lib.hs
+++ Hwhoami/src/Lib.hs
@@ -1,6 +1,36 @@
 module Lib
     ( someFunc
+      {-versionText-}
+      {-stripQuotes-}
+      {-getUsername  -}
     ) where
 
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStrLn    "someFunc 4"
+    putStrLn    "more text"
+
+stripQuotes :: String -> String
+stripQuotes ('"':xs) =
+  if last xs == '"'
+    then init xs
+    else '"' : xs
+stripQuotes xs = xs
+
+{-getUsername :: IO String
+getUsername = do
+  e <- getEnvironment
+  return (((snd . head) (filter (\x -> (fst x) == "USER") e)) ++ "\n")
+-}
+versionText :: [String]
+versionText =
+  [ "Hwhoami (Haskell implementation of GNU whoami) 1.0"
+  , "derrived from: whoami (GNU coreutils) 8.23"
+  , "Copyright (C) 2014 Free Software Foundation, Inc."
+  , "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+  , "This is free software: you are free to change and redistribute it."
+  , "There is NO WARRANTY, to the extent permitted by law."
+  , "Written by Richard Mlynarik."
+  , "Ported by PuZZleDucK.\n"
+  ]
